e6c685f0a4c2d37899af999166721164378ac860
Froggiesan <77341642+Froggiesan@users.noreply.github.com>
2021-05-10 21:24:13 +0200

    added viviendas

--- src/inmobiliaria_empesa/ConsBBDD.java
+++ src/inmobiliaria_empesa/ConsBBDD.java
@@ -137,17 +137,7 @@ public class ConsBBDD extends javax.swing.JFrame {
         Tabla.setModel(listarTrabajadorGap(Tabla));
         jScrollPane1.setViewportView(Tabla);
 
-        TablaValores.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
+        TablaValores.setModel(listarViviendasGap(TablaValores));
         jScrollPane3.setViewportView(TablaValores);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
